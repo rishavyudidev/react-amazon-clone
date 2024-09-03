4981fa41c0b53549f4c898ccff3b0e85718bd40f
rishavyudidev <rishavyudi.dev@gmail.com>
2024-09-03 20:20:10 +0530

    commit

---
+++ .vite/deps/@mui_icons-material_ArrowRight.js
@@ -0,0 +1,33 @@
+"use client";
+import {
+  require_createSvgIcon,
+  require_interopRequireDefault,
+  require_jsx_runtime
+} from "./chunk-AO3AVUWU.js";
+import "./chunk-S4244JTJ.js";
+import "./chunk-HC64NG5Q.js";
+import "./chunk-GYOI4P2W.js";
+import "./chunk-OTY6O57J.js";
+import "./chunk-2PA4WPI3.js";
+import {
+  __commonJS
+} from "./chunk-ROME4SDB.js";
+
+// node_modules/@mui/icons-material/ArrowRight.js
+var require_ArrowRight = __commonJS({
+  "node_modules/@mui/icons-material/ArrowRight.js"(exports) {
+    var _interopRequireDefault = require_interopRequireDefault();
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    exports.default = void 0;
+    var _createSvgIcon = _interopRequireDefault(require_createSvgIcon());
+    var _jsxRuntime = require_jsx_runtime();
+    var _default = (0, _createSvgIcon.default)((0, _jsxRuntime.jsx)("path", {
+      d: "m10 17 5-5-5-5v10z"
+    }), "ArrowRight");
+    exports.default = _default;
+  }
+});
+export default require_ArrowRight();
+//# sourceMappingURL=@mui_icons-material_ArrowRight.js.map
